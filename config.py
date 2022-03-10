16a0cd6298effe551f6b06c78ed7be04b70229cf
John-Kimani <kimwaweru100@gmail.com>
2022-03-10 09:27:22 +0300

    Fix: Able to run app

--- config.py
+++ config.py
@@ -11,8 +11,10 @@ class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
-    DEBUG = True 
-
+    '''
+    Development class
+    '''
+    DEBUG=True
 config_options = {
     'development':DevConfig,
     'production':ProdConfig
