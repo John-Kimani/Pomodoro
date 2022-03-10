2fee441aef4912446b59feca47f6681b20a77ede
John-Kimani <kimwaweru100@gmail.com>
2022-03-10 11:42:28 +0300

    Dev: Create pomodoro timer break logic

---
+++ app/main/p-timer.py
@@ -0,0 +1,45 @@
+import time
+
+
+
+
+
+print("Pomodoro starts now!")
+'''
+Traditional technique to manage time in work and studies.
+Timer starts with 25-minutes and 5-minutes intervals
+Methods: 
+    Loop to 
+'''
+for i in range(4):
+    goTiktok = 25*60
+    '''
+    Convers 25 minutes to 1500 seconds
+    '''
+    while goTiktok:
+        mins = goTiktok // 60
+        '''
+        for division converting time into minutes
+        '''
+        secs = goTiktok % 60
+        '''
+        Modulous to from help find remainder
+        '''
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        '''
+        Format for size (2 width, 0 padding left) and d for a digit value
+        '''
+        print(timer, end='\r')
+        '''
+        Print timer and help avoid ovewriting timer
+        '''
+        time.sleep(1)
+        '''
+        Timer to sleep for a second before count down 
+        '''
+        goTiktok -=1
+        '''
+        Decrement timer by one second
+        '''
+print('Break Time!!')
+        
