eb2d65d31ec7a518edd1e54cfee6beadbf87eece
John-Kimani <kimwaweru100@gmail.com>
2022-03-10 11:56:25 +0300

    Dev: Creates back to work logic

--- app/main/p-timer.py
+++ app/main/p-timer.py
@@ -19,11 +19,11 @@ for i in range(4):
     while goTiktok:
         mins = goTiktok // 60
         '''
-        for division converting time into minutes
+        floor division converting time into minutes
         '''
         secs = goTiktok % 60
         '''
-        Modulous to from help find remainder
+        Modulous to from help find remainder in seconds
         '''
         timer = '{:02d}:{:02d}'.format(mins, secs)
         '''
@@ -42,4 +42,37 @@ for i in range(4):
         Decrement timer by one second
         '''
 print('Break Time!!')
+'''
+Five minutes break
+'''
+goTiktok = 5*60
+'''
+    Converts five minutes to seconds
+'''
+while goTiktok:
+    '''
+    Loop to execute countdown
+    '''
+    mins = goTiktok // 60
+    '''
+    floor division converting time into minutes
+    '''
+    secs = goTiktok % 60
+    '''
+        Modulous to from help find remainder in seconds
+    '''
+    timer = '{:02d}:{:02d}'.format(mins, secs)
+    '''
+        Format for size (2 width, 0 padding left) and d for a digit value
+    '''
+    print(timer, end='\r')
+    '''
+        Print timer and help avoid ovewriting timer
+    '''
+    time.sleep(1)
+    '''
+        Timer to sleep for a second before count down 
+    '''
+    goTiktok -=1
+print('Work Time!')
         
